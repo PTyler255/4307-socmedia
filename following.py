b2904c32787e9986c281a2aa139ff33e00a05a66
Ian <tekoxvi@gmail.com>
2022-02-15 13:30:57 -0700

    added mains

--- following.py
+++ following.py
@@ -3,11 +3,17 @@ import sys
 
 # ./following.py my_username their_username
 
-(from_id, following_id) = sys.argv[1:3]
 
-con = sqlite3.connect('blueit.db')
-cur = con.cursor()
+def follow(from_id, following_id):
+    con = sqlite3.connect('blueit.db')
+    cur = con.cursor()
+    cur.execute('INSERT INTO following (from_id, following_id) VALUES (?, ?)', [from_id, following_id])
+    con.commit()
+    con.close()
 
-cur.execute('INSERT INTO following (from_id, following_id) VALUES (?, ?)', [from_id, following_id])
-con.commit()
-con.close()
+def main():
+    (from_id, following_id) = sys.argv[1:3]
+    follow(from_id, following_id)
+
+if __name__ == "__main__":
+    main()
